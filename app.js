453c5c2630976b9174dad04c59864eb26f7fda8b
Marvin Deutz <marvin.deutz@yahoo.com>
2023-02-23 09:57:51 +0100

    Node weather project

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+import axios from "axios";
+
+// Load env variables from .env into process.env
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+const city = "berlin";
+
+app.get("/weather", async (req, res) => {
+    // Proxy Server
+    const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`
+    );
+    res.send(response.data);
+});
+
+app.listen(PORT, () => console.log("Server is running on port:", PORT));
